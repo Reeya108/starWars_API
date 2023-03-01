4cb07016dcf700bec3d23719ea1921d938638dde
Manasi <rranade669@gmail.com>
2023-03-01 23:50:28 +0530

    All done

--- resources/films.py
+++ resources/films.py
@@ -4,7 +4,17 @@ from typing import Dict
 
 
 class Film(ResourceBase):
+    """
+    Film class related functionality
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.relative_url = "/api/films"
+
     def get_count(self):
+        complete_url = self.home_url + self.relative_url
+        response = hit_url(complete_url)
         data = response.json()
         count = data.get("count")
         return count
--- task_four.py
+++ task_four.py
@@ -1,21 +1,21 @@
 """
 TODO
 1. Pull data for the first movie ("A New Hope") and save in DB.
-
 2. Replace the data for each endpoint listed in the JSON object and insert
 that data into database table
-
 For example - "A new hope" movie has following resource endpoints -
-
-- characters
-- planets
-- starships
-- vehicles
-- species
+- characters 15
+- planets  7
+- starships   10
+- vehicles  5
+- species  40
 """
 
 from resources.films import Film   # resource model
 from models.datamodels.films import Film_  # pydantic model
+
+from dal.db_conn_helper import get_db_conn
+from dal.dml import insert_resource
 
 
 if __name__ == "__main__":
@@ -23,5 +23,43 @@ if __name__ == "__main__":
     film_data = Film_(**data)
 
     # create DB connection
-    # to create a sql query to insert data into database.
-    # execute the query
+    conn = get_db_conn()
+
+    film_columns = [
+        "title",
+        "opening_crawl",
+        "director",
+        "producer",
+        "release_date",
+        "created",
+        "edited",
+        "url",
+    ]
+
+    film_values = [
+        film_data.title,
+        film_data.opening_crawl,
+        film_data.director,
+        film_data.producer,
+        film_data.release_date,
+        film_data.created.strftime("%y-%m-%d"),
+        film_data.edited.strftime("%y-%m-%d"),
+        film_data.url,
+    ]
+    breakpoint()
+
+    result = insert_resource(
+        "film", "film_id", film_data.episode_id, film_columns, film_values
+    )
+
+    # TODO
+    # capture all characters
+    # film_data.characters
+    # only values will change
+    # column list can be once created and re-used
+
+    # TODO
+    # capture all planets
+    # film_data.planets
+    # only values will change
+    # column list can be once created and re-used
